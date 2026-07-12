e09d88abdb9d4044eb0d184911ba2497a663e93e
Kavindu-Madhushanka <madu10275@gmail.com>
2026-07-12 17:48:29 +0530

    add interface of auth repository

---
+++ Repositories/IAuthRepository.cs
@@ -0,0 +1,12 @@
+﻿using eduasst_backend.DTOs;
+using eduasst_backend.Models;
+using Microsoft.EntityFrameworkCore.Internal;
+namespace eduasst_backend.Repositories
+{
+    public interface IAuthRepository
+    {
+        Task<Users> Register(SignUp request);
+        Task<bool> UserCheck(string email);
+        Task<Users> Login(SignIn request);
+    }
+}
