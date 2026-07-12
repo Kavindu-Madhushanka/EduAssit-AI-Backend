d3432ef47364f1806427fcf12d9cf61507bc96ca
Kavindu-Madhushanka <madu10275@gmail.com>
2026-07-12 17:47:34 +0530

    modify

--- Models/Users.cs
+++ Models/Users.cs
@@ -15,5 +15,8 @@ namespace eduasst_backend.Models
         public string PasswordHash { get; set; } = string.Empty;
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public string FullName { get; set; }= string.Empty;
     }
 }
