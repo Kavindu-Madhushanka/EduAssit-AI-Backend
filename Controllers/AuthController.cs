a101ec19e486c787ced19c0953c22113ba5e75a0
Kavindu-Madhushanka <madu10275@gmail.com>
2026-07-12 17:46:59 +0530

    modify

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,20 +1,58 @@
 ﻿using eduasst_backend.Data;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using eduasst_backend.DTOs;
 using eduasst_backend.Models;
+using eduasst_backend.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eduasst_backend.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController : ControllerBase
-    {
-        private readonly AddDbContext _context;
-
-        public AuthController(AddDbContext context) { 
-            _context = context;
+    public class AuthController : ControllerBase {
+        private readonly IAuthRepository _authRepo;
+        public AuthController(IAuthRepository authRepo) {
+            _authRepo = authRepo;
         }
+        [HttpPost("signup")]
+        public async Task<IActionResult> Register([FromBody] SignUp request) {
+            try {
+                Users createdUser = await _authRepo.Register(request);
+                return Ok(new
+                {
+                    message = "User registered successfully!",
+             
+                    email = createdUser.Email,
+                    userid=createdUser.Id
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpPost("signin")]
+        public async Task<IActionResult> Login([FromBody] SignIn request) {
+            try
+            {
+                Users loginuser = await _authRepo.Login(request);
+                return Ok(new
+                {
+                    message = "User login successfully!",
+                    email = loginuser.Email,
+                    userid=loginuser.Id
+                });
+            }
+            catch (Exception ex) {
+                return BadRequest(new { message = ex.Message });
+            }
+           
+             
+        }
+
+        
     }
 }
