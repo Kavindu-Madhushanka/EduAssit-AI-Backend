8f815c5a6d89156ca94cfa62b17f429b4582033e
Kavindu-Madhushanka <madu10275@gmail.com>
2026-07-15 01:14:59 +0530

    add subject folder model

---
+++ Models/SubjectFolder.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eduasst_backend.Models
+{
+    public class SubjectFolder
+    {
+        [Key]
+        public int FolderID { get; set; }
+
+        [Required]
+        public int UserID { get; set; }
+
+        [Required]
+        public string FolderName { get; set; }=String.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+
+    }
+}
