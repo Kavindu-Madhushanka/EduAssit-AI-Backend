c486e9ce69176b7c2114f4763a628f53e67252be
Kavindu-Madhushanka <madu10275@gmail.com>
2026-07-15 01:14:39 +0530

    add folder DTO

---
+++ DTOs/Folder.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eduasst_backend.DTOs
+{
+    public class Folder
+    {
+        
+        public int UserID { get; set; }
+
+        
+        public string FolderName { get; set; } = String.Empty;
+    }
+}
