293d545381aef8f510df0d807ce0d49f87e5e2bd
Kavindu-Madhushanka <madu10275@gmail.com>
2026-07-15 01:15:57 +0530

    add new interface

---
+++ Repositories/IFolderLessonNotesRepository.cs
@@ -0,0 +1,10 @@
+﻿using eduasst_backend.DTOs;
+using eduasst_backend.Models;
+namespace eduasst_backend.Repositories
+{
+    public interface IFolderLessonNotesRepository
+    {
+        Task<List<SubjectFolder>> CreateFolder(Folder request);
+        Task<bool>CheckFolder(string  folderName);
+    }
+}
